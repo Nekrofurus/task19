ad3c55350ef47f40484ea68e1cbe48cf7a8c8ffb
VVCephei <124810683+Nekrofurus@users.noreply.github.com>
2023-03-09 15:45:29 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// Задача 19
+// Задача 19
 // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 // 14212 -> нет
 // 12821 -> да
@@ -7,33 +7,29 @@ Console.WriteLine("Для проверки является ли число па
 int number = Convert.ToInt32(Console.ReadLine());
 bool ControlFiveNumbers(int number)
 {
-    if (number <= 99999 || number > 10000)
+    if (number >= 99999 || number < 10000)
     {
-                return true;
+            return true;
     }
     Console.WriteLine("Ошибка! Введено не пятизначное число!");
     return false;
 }
-int n1 = number / 10000;
-int n2 = number / 1000 % 10;
-int n3 = number % 10;
-int n5 = number/10 % 10;
+int number1 = number / 10000;
+int number2 = number / 1000 % 10;
+int number3 = number % 10;
+int number5 = number/10 % 10;
 bool Palindrome (int n1, int n2, int n3, int n5)
 {
     if (n1 == n5 && n2 == n3)
     {
-        Console.WriteLine("Ошибка! Введено не пятизначное число!");
-        return false;
+        return true;
     }
-    return true;
+    return false;
 }
 if (ControlFiveNumbers(number))
 {
-if (Palindrome (n1, n2, n3, n5))
-    {
+    if (Palindrome (number1, number2, number3, number5))
         Console.WriteLine($"Да,число {number}  - палиндром");
-    }
     else
-    Console.WriteLine($"Нет,число {number}  -  не палиндром");
+        Console.WriteLine($"Нет,число {number}  -  не палиндром");
 } 
-
