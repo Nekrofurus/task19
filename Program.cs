566cacd75a69ece48a7d7bb33b8e7b5e5f6a7a3f
VVCephei <124810683+Nekrofurus@users.noreply.github.com>
2023-03-13 16:08:36 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,22 +5,23 @@
 // 23432 -> да
 Console.WriteLine("Для проверки является ли число палиндромом введите пятизначное число");
 int number = Convert.ToInt32(Console.ReadLine());
-bool ControlFiveNumbers(int number)
+bool ControlFiveNumbers(int num)
 {
-    if (number >= 99999 || number < 10000)
+    if (num >= 10000 && num <= 99999)
     {
-            return true;
+        return true;
     }
     Console.WriteLine("Ошибка! Введено не пятизначное число!");
     return false;
 }
 int number1 = number / 10000;
 int number2 = number / 1000 % 10;
-int number3 = number % 10;
-int number5 = number/10 % 10;
-bool Palindrome (int n1, int n2, int n3, int n5)
+int number5 = number % 10;
+int number4 = number / 10 % 10;
+
+bool Palindrome(int n1, int n2, int n4, int n5)
 {
-    if (n1 == n5 && n2 == n3)
+    if (n1 == n5 && n2 == n4)
     {
         return true;
     }
@@ -28,8 +29,8 @@ bool Palindrome (int n1, int n2, int n3, int n5)
 }
 if (ControlFiveNumbers(number))
 {
-    if (Palindrome (number1, number2, number3, number5))
-        Console.WriteLine($"Да,число {number}  - палиндром");
+    if (Palindrome(number1, number2, number4, number5))
+        Console.WriteLine($"Да,число {number} - палиндром");
     else
-        Console.WriteLine($"Нет,число {number}  -  не палиндром");
-} 
+        Console.WriteLine($"Нет,число {number} - не палиндром");
+}
